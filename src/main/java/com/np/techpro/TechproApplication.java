7c19f73455a02f555f6bd22258351cdd4c07d946
Nimish <nimishtechpro@gmail.com>
2022-06-12 14:19:12 -0600

    NTP-103 - Implement logback logging for spring boot

--- src/main/java/com/np/techpro/TechproApplication.java
+++ src/main/java/com/np/techpro/TechproApplication.java
@@ -1,19 +1,23 @@
 package com.np.techpro;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechproApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(TechproApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(TechproApplication.class, args);
-		System.out.println("=== Application Started ! ===");
-		System.out.println("URL : http://localhost:8080/api/v1/welcome ");
-		System.out.println("Swagger UI : http://localhost:8080/api/v1/swagger-ui/index.html");
-		System.out.println("H2 Database console : http://localhost:8080/api/v1/h2-console/");
-		System.out.println("Database console : username : sa , Password : password");
-		System.out.println("================================");
+
+		logger.info("=== Application Started ! ===");
+		logger.info("URL : http://localhost:8080/api/v1/welcome ");
+		logger.info("Swagger UI : http://localhost:8080/api/v1/swagger-ui/index.html");
+		logger.info("H2 Database console : http://localhost:8080/api/v1/h2-console/");
+		logger.info("Database console : username : sa , Password : password");
+		logger.info("================================");
 	}
 
 }
