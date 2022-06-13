506b6097a54cb89f147588679f0d9fdb1d8a768e
Nimish <nimishtechpro@gmail.com>
2022-06-12 23:56:19 -0600

    NTP-104 : Added integration test with Rest Assured

--- src/main/java/com/np/techpro/controller/UserController.java
+++ src/main/java/com/np/techpro/controller/UserController.java
@@ -3,6 +3,7 @@ package com.np.techpro.controller;
 import com.np.techpro.dto.User;
 import com.np.techpro.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,12 @@ public class UserController {
         enitityUser.setLastName(user.getLastName());
         com.np.techpro.model.User result= userRepository.save(enitityUser);
         return result.getId();
+    }
+    @GetMapping("users/list")
+    public User getAllUsers(){
+        User user = new User();
+        user.setFirstName("nimish");
+        user.setLastName("pp");
+        return user;
     }
 }
---
+++ src/test/java/com/np/techpro/controller/UserControllerTest.java
@@ -0,0 +1,27 @@
+package com.np.techpro.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import static io.restassured.RestAssured.get;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+
+    //This annotation will get the currently running port
+    @LocalServerPort
+    private int port;
+
+
+    @Test
+    public void testUser() {
+
+        get("http://localhost:"+port+"/api/v1/users/list")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("firstName", Matchers.equalTo("nimish"));
+    }
+
+}
